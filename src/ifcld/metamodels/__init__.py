0c398d4da58de1f69c2a09d60f2ae0a5b94f5f07
Argentina Ortega Sáinz <argenos@users.noreply.github.com>
2025-12-08 11:46:32 +0100

    Add default metamodel path to package

--- src/ifcld/metamodels/__init__.py
+++ src/ifcld/metamodels/__init__.py
@@ -2,3 +2,7 @@ import os
 import json
 
 _current_path = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(_current_path, "ifc-metamodel.json")) as f:
+    METAMODEL = json.load(f)
+with open(os.path.join(_current_path, "prov.json")) as f:
+    PROV_METAMODEL = json.load(f)
--- src/ifcld/transformations.py
+++ src/ifcld/transformations.py
@@ -2,6 +2,7 @@ import os
 
 from ifcld.interpreters.jsonld import Interpreter
 from ifcld.utils import save_jsonld_file, load_json
+from ifcld.metamodels import PROV_METAMODEL, METAMODEL
 
 FPM_MODEL_BASE_URI = "https://secorolab.github.io/models/"
 
@@ -15,16 +16,18 @@ def get_jsonld_interpretation(file_path):
 
 def load_context(model_name, context_path=None):
     if context_path is None:
-        context_path = "metamodels/ifc-metamodel.json"
-    context = load_json(context_path)
-    context["@context"]["ifc-model"] = context["@context"]["ifc-model"].replace(
-        "bim_model_id", model_name.replace(" ", "-")
+
+        context = dict(**METAMODEL)
+    else:
+        context = load_json(context_path)
+
+    context["@context"]["ifc-model"] = (
+        FPM_MODEL_BASE_URI + model_name.replace(" ", "-") + "/ifc/data#"
     )
-    print(context["@context"]["ifc-model"])
     return context
 
 
-def transform_ifc_to_jsonld(model_path, output_dir):
+def transform_ifc_to_jsonld(model_path, output_dir, file_name=None, save_prov=False):
     model_name = os.path.basename(model_path).lower().replace(".ifc", "")
     ifc_model, prov = get_jsonld_interpretation(model_path)
 
@@ -34,7 +37,9 @@ def transform_ifc_to_jsonld(model_path, output_dir):
 
     # ifc_model.pop(0)
 
-    file_name = "{}.ifc.json".format(model_name)
+    if file_name is None:
+        file_name = "{}.ifc.json".format(model_name)
+
     save_jsonld_file(
         os.path.join(output_dir, file_name),
         ifc_model,
@@ -42,14 +47,15 @@ def transform_ifc_to_jsonld(model_path, output_dir):
         # graph_id="model:ifc",
     )
 
-    prov_context = load_json("metamodels/prov.json")
-    prov_context["@context"]["fpm-model"] = model_iri
+    if save_prov:
+        prov_context = dict(**PROV_METAMODEL)
+        prov_context["@context"]["fpm-model"] = model_iri
 
-    file_name = "{}.ifc.prov.json".format(model_name)
-    save_jsonld_file(
-        os.path.join(output_dir, file_name),
-        prov,
-        context=prov_context,
-        graph_id="fpm-model:prov",
-    )
+        file_name = "{}.ifc.prov.json".format(model_name)
+        save_jsonld_file(
+            os.path.join(output_dir, file_name),
+            prov,
+            context=prov_context,
+            graph_id="fpm-model:prov",
+        )
     return model_name
